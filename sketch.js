9c79e69ba5de96254c90045fcf8bace08c6de156
Ricardo Sander Lopes <ricardo.lopes@ifood.com>
2020-06-23 14:00:05 -0300

    Primeiro sketch: cenário andando.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let setImage;
+
+let x1;
+let x2;
+let y;
+let speed;
+
+function preload() {
+    setImage = loadImage('assets/images/set/forest.png');
+}
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+    createCanvas(windowWidth, windowHeight);
+    x1 = 0;
+    x2 = width;
+    y = 0;
+    speed = 10;
+}
+
+function draw() {
+    image(setImage, x1, y, width, height);
+    image(setImage, x2, y, width, height);
+    
+    x1 -= speed;
+    x2 -= speed;
+
+    if (x1 <= -width) {
+        x1 = width;
+    }
+
+    if (x2 <= -width) {
+        x2 = width;
+    }
+}
